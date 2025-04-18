55a9900578545be803f4a3c1c1e9ec0317df65d6
rht-arch <rafaharotello2003@gmail.com>
2025-04-18 18:21:45 +0200

    Primeras interfaces y controladores, pruebas con la api de Gmail

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module org.example.guia {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires jdk.httpserver;
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires google.api.client;
+    requires com.google.api.client;
+    requires com.google.api.client.json.gson;
+    requires com.google.api.services.gmail;
+    requires com.google.api.client.extensions.jetty.auth;
+    requires com.google.api.client.auth;
+    requires com.google.api.client.extensions.java6.auth;
+
+    opens org.example.guia to javafx.fxml;
+    exports org.example.guia;
+}
---
+++ src/main/java/org/example/guia/Registro.java
@@ -0,0 +1,4 @@
+package org.example.guia;
+
+public class Registro {
+}
